a47f54cf9eff61eb9d1856d2d07ee5fb63af676b
Thejaswi Muniyappa <thejaswi@dyn-160-39-132-175.dyn.columbia.edu>
2017-04-13 21:20:33 -0400

    Email Verification

---
+++ EmailVerification/verifyEmail.py
@@ -0,0 +1,41 @@
+import boto3
+import hashlib
+import time
+
+def verify_email(event, response):
+    dynamodb = boto3.resource('dynamodb', region_name = "us-west-2")
+    table = dynamodb.Table('Customer')
+    entry = table.get_item(
+        Key={
+            'id': event['id'],
+        }
+    )
+    # print entry
+    
+    if entry['Item']['verified']:
+        response["success"] = "True"
+        response["AlreadyVerified"] = "True"
+        return response
+
+
+    if entry['Item']['hash_val'] and (entry['Item']['hash_val'] == event['hash_val']):
+        table.update_item(
+            Key={
+                'id': event['id'],
+            },
+            UpdateExpression="set verified = :r",
+            ExpressionAttributeValues={
+                ':r': bool(1)
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        response["success"] = "True"
+    else:
+        response["success"] = "False"
+    return response
+    
+
+def lambda_handler(event, context):
+    response = dict()
+    response['id'] = event['id']
+    return verify_email(event, response)
